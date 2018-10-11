3a731059ed948f622d443e34cfda27b2fb5f03b2
siarhei-rachynski-epam <siarhei_rachynski@epam.com>
2018-10-11 17:51:37 +0300

    second more line added

--- src/com/testproject/Main.java
+++ src/com/testproject/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("test project");
         System.out.println("one more line");
+        System.out.println("second more line added");
     }
 }
