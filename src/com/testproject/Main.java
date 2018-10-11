2c68286b9dc9a6d4193bee534b4140713cddc9f3
home282 <sergei.rachinski@gmail.com>
2018-10-11 22:44:12 +0300

    one more extra line added

--- src/com/testproject/Main.java
+++ src/com/testproject/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("test project");
         System.out.println("one more line");
         System.out.println("extra line added directly on github");
+        System.out.println("one more extra line added from home282");
     }
 }
