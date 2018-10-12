44acb17a5080de420e860ce67a5721e4a2983389
home282 <sergei.rachinski@gmail.com>
2018-10-13 00:29:50 +0300

    feature 2: one more commit

--- src/com/testproject/SimpleFeature2.java
+++ src/com/testproject/SimpleFeature2.java
@@ -4,5 +4,6 @@ public class SimpleFeature2 {
 
     public void SimpleFunction() {
         System.out.println("SimpleFeature2 -> simple function");
+        System.out.println("One more commit into feature 2");
     }
 }
