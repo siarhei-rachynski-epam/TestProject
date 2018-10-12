90d7379b1bc747cecd932c23100ca8abbb2451cc
home282 <sergei.rachinski@gmail.com>
2018-10-13 00:11:51 +0300

    feature 1: adding simple function

--- src/com/testproject/Main.java
+++ src/com/testproject/Main.java
@@ -6,5 +6,11 @@ public class Main {
         System.out.println("test project");
         System.out.println("one more line");
         System.out.println("extra line added directly on github");
+
+        SimpleFeature1 sf1 = new SimpleFeature1();
+
+        sf1.SimpleFunction();
     }
+
+
 }
