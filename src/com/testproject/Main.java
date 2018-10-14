e17199e6ac833baeaccddfad35fb0fd5509df84f
home282 <sergei.rachinski@gmail.com>
2018-10-14 22:26:55 +0300

    test stashing - staged or committed into local but not pushed to remote

--- src/com/testproject/Main.java
+++ src/com/testproject/Main.java
@@ -30,6 +30,9 @@ public class Main {
 
         System.out.println("feature200v2 - worked on some urgent patch");
 
+        System.out.println("feature200v2 - test stashing - staged (committed into local)");
+
+
         System.out.println("in master to test rebasing");
 
         System.out.println("test rebasing");
