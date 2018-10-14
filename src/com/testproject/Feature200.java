9dd74da3a8889ae7826ef250508bd3efe9109dd1
home282 <sergei.rachinski@gmail.com>
2018-10-14 12:18:31 +0300

    feature 200 - created class for feature

---
+++ src/com/testproject/Feature200.java
@@ -0,0 +1,10 @@
+package com.testproject;
+
+public class Feature200 extends SimpleFeature1  {
+
+    public void SomeOp() {
+        SimpleFunction();
+
+        System.out.println("some operation in feature 200");
+    }
+}
